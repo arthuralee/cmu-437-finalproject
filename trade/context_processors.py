922a6eb9bd60a5d313052061d40f2da3cdbf4bee
Arthur Lee <arthur@arthurlee.me>
2014-04-23 14:11:13 -0400

    Improved login & registration, template cleanup

--- trade/context_processors.py
+++ trade/context_processors.py
@@ -20,10 +20,14 @@ def nav(request):
   nav = {}
   if request.path == "/":
     nav['home'] = True
-  elif request.user.username in request.path:
+  elif 'profile' in request.path and request.user.username in request.path:
     nav['profile'] = True
   elif 'manage' in request.path:
     nav['catalog'] = True
   elif 'trade' in request.path:
     nav['trade'] = True
+  elif 'login' in request.path:
+    nav['login'] = True
+  elif 'register' in request.path:
+    nav['register'] = True
   return {'nav': nav}
