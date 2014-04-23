8d354d59c0f02d134d5fbedb197803bf58200618
Arthur Lee <arthur@arthurlee.me>
2014-04-23 14:57:10 -0400

    Updated database sample data

--- app/settings.py
+++ app/settings.py
@@ -26,8 +26,6 @@ DATABASES = {
     }
 }
 
-CACHE_MIDDLEWARE_ANONYMOUS_ONLY=True
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
